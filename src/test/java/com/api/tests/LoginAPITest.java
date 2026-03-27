016a8744ffdcefc961a75de74ca8746dbabc6850
BernardoSJ <salinasjaquezb@gmail.com>
2026-03-27 16:46:03 -0600

    Add initial files

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,12 @@
+package com.api.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginAPITest {
+
+    @Test(description = "Verify if Login API is working...")
+    public void loginTest(){
+        
+    }
+
+}
