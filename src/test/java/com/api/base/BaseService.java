7975cf904527aff2200ce24b2af8d33472044b85
BernardoSJ <salinasjaquezb@gmail.com>
2026-03-28 22:30:18 -0600

    Add Improvements to Base Service, Auth Service and Login Initial test

--- src/test/java/com/api/base/BaseService.java
+++ src/test/java/com/api/base/BaseService.java
@@ -14,7 +14,7 @@ public class BaseService {
         requestSpecification = RestAssured.given().baseUri(BASE_URL);
     }
 
-    protected Response postRequest(String payload, String endpoint){
+    protected Response postRequest(Object payload, String endpoint){
         return requestSpecification.contentType(ContentType.JSON).body(payload).post(endpoint);
     }
 
---
+++ src/test/java/com/api/tests/LoginAPITest3.java
@@ -0,0 +1,27 @@
+package com.api.tests;
+
+import com.api.base.AuthService;
+import com.api.models.request.LoginRequest;
+import com.api.models.response.LoginResponse;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+public class LoginAPITest3 {
+
+    @Test(description = "Verify if Login API is working...")
+    public void loginTest(){
+        LoginRequest loginRequest = new LoginRequest("BernardoSJ", "Bernardo$1998");
+        AuthService authService = new AuthService();
+        Response response = authService.login(loginRequest);
+        LoginResponse loginResponse = response.as(LoginResponse.class);
+
+        Assert.assertTrue(loginResponse.getToken() != null);
+        Assert.assertEquals(loginResponse.getEmail(), "bernardo.salinas.jaquez.qa@gmail.com");
+        Assert.assertEquals(loginResponse.getId(), 4841);
+    }
+
+}
